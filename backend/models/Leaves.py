aec7083710ff966e87db101ef01394eab3f95d4d
Aldrin0727 <aldrin.canarejo@jewelmer.com>
2025-12-01 16:18:39 +0800

    test

--- backend/models/Leaves.py
+++ backend/models/Leaves.py
@@ -4,23 +4,6 @@
 
 # create_leave_bp = Blueprint('create_leave', __name__)
 
-<<<<<<< HEAD
-@create_leave_bp.route('/leave_details', methods=['POST'])
-def get_leave_details():
-    try:
-        data = request.get_json()
-
-
-
-
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * from Leave_Details")
-        leave_details = cursor.fetchall()
-        cursor.close()
-        return jsonify(leave_details), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}),500
-=======
 # @create_leave_bp.route('/leave_details', methods=['POST'])
 # def get_leave_details():
 #     try:
@@ -32,5 +15,4 @@ def get_leave_details():
 #         return jsonify(leave_details), 200
 #     except Exception as e:
 #         return jsonify({"error": str(e)}),500
->>>>>>> origin/lj_branch
 
