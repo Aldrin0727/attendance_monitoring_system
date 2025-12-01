4afce42e1a8add42a1dec3476e863bcf9dd42cdd
Aldrin0727 <aldrin.canarejo@jewelmer.com>
2025-12-01 16:58:41 +0800

    Co-authored-by: ljisagunde <ljisagunde@users.noreply.github.com>

--- backend/config.py
+++ backend/config.py
@@ -26,5 +26,6 @@ class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST')
     MYSQL_USER = os.getenv('MYSQL_USER')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
     MYSQL_DB2 = os.getenv('MYSQL_DB2')
-    MYSQL_DB = os.getenv('MYSQL_DB1')
+    
